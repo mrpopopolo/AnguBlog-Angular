5c63872c823d3729fc2f95df94cb078c2c661bd1
mrpopopolo <popopolo@gmail.com>
2021-10-24 11:37:24 +0200

    Updated angular version

--- src/app/article-form/article-form.component.spec.ts
+++ src/app/article-form/article-form.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { ArticleFormComponent } from './article-form.component';
 
@@ -6,7 +6,7 @@ describe('ArticleFormComponent', () => {
   let component: ArticleFormComponent;
   let fixture: ComponentFixture<ArticleFormComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ ArticleFormComponent ]
     })
--- src/app/article-update/article-update.component.spec.ts
+++ src/app/article-update/article-update.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { ArticleUpdateComponent } from './article-update.component';
 
@@ -6,7 +6,7 @@ describe('ArticleUpdateComponent', () => {
   let component: ArticleUpdateComponent;
   let fixture: ComponentFixture<ArticleUpdateComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ ArticleUpdateComponent ]
     })
